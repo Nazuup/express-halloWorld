866ff2fa0ad3d7bb779c3392765d1d2620ee0c94
Nazuup <166776126+Nazuup@users.noreply.github.com>
2024-06-21 11:05:12 +0900

    hello修正、index.html追加

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 3001;
 
 app.get("/hello", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () =>
+  
+  console.log(`Example app listening on port ${port}!`)
+);
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -58,4 +61,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
